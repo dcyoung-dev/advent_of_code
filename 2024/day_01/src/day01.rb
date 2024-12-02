0805e437b655b994b10a61c6bec97f4d846ea9cb
David Young <david@dcyoung.dev>
2024-12-02 17:31:49 +0000

    Refactored day 01 part one

--- 2024/day_01/src/day01.rb
+++ 2024/day_01/src/day01.rb
@@ -2,44 +2,26 @@
 
 class Day01
   def initialize(file)
-    @lines = file.readlines.map(&:chomp)
+    lines = file.readlines.map(&:chomp)
+    left_array = []
+    right_array = []
+
+    lines.each do |line|
+      left, right = line.scan(/\d+/)
+
+      left_array << left.to_i
+      right_array << right.to_i
+    end
+
+    @left_array = left_array.sort
+    @right_array = right_array.sort
   end
 
   def part_one
-    left_array = []
-    right_array = []
-
-    @lines.each do |line|
-      left, right = line.scan(/\d+/)
-
-      left_array << left.to_i
-      right_array << right.to_i
-    end
-
-    total_distance = 0
-    left_array.sort.zip(right_array.sort).each do |left, right|
-      distance = right - left
-      total_distance += if distance.positive?
-                          distance
-                        else
-                          distance * -1
-                        end
-    end
-
-    total_distance
+    left_array.zip(right_array).inject(0) { |sum, (left, right)| sum + distance_between(left, right) }
   end
 
   def part_two
-    left_array = []
-    right_array = []
-
-    @lines.each do |line|
-      left, right = line.scan(/\d+/)
-
-      left_array << left.to_i
-      right_array << right.to_i
-    end
-
     similarities = left_array.map do |left|
       # number of time left appears in the right array
       multiplier = right_array.count { |right| right == left }
@@ -49,4 +31,13 @@ class Day01
 
     similarities.sum
   end
+
+  private
+
+  def distance_between(left, right)
+    raw_distance = right - left
+    raw_distance.positive? ? raw_distance : raw_distance * -1
+  end
+
+  attr_reader :left_array, :right_array
 end
